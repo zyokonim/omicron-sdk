7618fde291ea3c68a30c47ea76d25d841849f210
febret <febret@gmail.com>
2012-04-19 03:39:02 +0000

    NEW: DirectXInputService button Up / Down events

--- include/omicron/DirectXInputService.h
+++ include/omicron/DirectXInputService.h
@@ -33,38 +33,6 @@
 #include "omicron/ServiceManager.h"
 #include "omicron/dinput.h"
 
-//#include "resource.h" // contained defines below
-#define IDI_MAIN                        102
-#define IDD_JOYST_IMM                   103
-#define IDR_ACCELERATOR1                103
-#define IDC_CLOSE                       1001
-#define IDC_X_AXIS                      1010
-#define IDC_Y_AXIS                      1011
-#define IDC_Z_AXIS                      1012
-#define IDC_X_AXIS_TEXT                 1013
-#define IDC_Y_AXIS_TEXT                 1014
-#define IDC_Z_AXIS_TEXT                 1015
-#define IDC_X_ROT_TEXT                  1016
-#define IDC_Y_ROT_TEXT                  1017
-#define IDC_Z_ROT_TEXT                  1018
-#define IDC_SLIDER0_TEXT                1019
-#define IDC_X_ROT                       1020
-#define IDC_Y_ROT                       1021
-#define IDC_Z_ROT                       1022
-#define IDC_SLIDER1_TEXT                1023
-#define IDC_POV0_TEXT                   1024
-#define IDC_POV1_TEXT                   1025
-#define IDC_POV2_TEXT                   1026
-#define IDC_POV3_TEXT                   1027
-#define IDC_SLIDER0                     1030
-#define IDC_SLIDER1                     1031
-#define IDC_POV                         1040
-#define IDC_POV0                        1040
-#define IDC_BUTTONS                     1041
-#define IDC_POV1                        1042
-#define IDC_POV2                        1043
-#define IDC_POV3                        1044
-
 namespace omicron
 {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -91,6 +59,8 @@ public:
 private:
 	static DirectXInputService* mysInstance;
 	float myUpdateInterval;
+	// 4 = max number of controllers.
+	uint myButtonState[4];
 };
 
 }; // namespace omicron
